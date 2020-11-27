21e68876615ba55542e44929ce332792bbeb9801
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-11-27 18:30:20 +0200

    Preliminary build

---
+++ jheaps/__init__.py
@@ -0,0 +1,39 @@
+"""
+Python-JHeaps Library
+~~~~~~~~~~~~~~~~~~~~~
+
+JHeaps is a library providing state-of-the-art heap data structures.
+
+See https://github.com/d-michail/python-jheaps for more details.
+"""
+
+from .__version__ import __title__, __description__, __url__
+from .__version__ import __version__, __backend_version__
+from .__version__ import __author__, __author_email__, __license__
+from .__version__ import __copyright__
+
+# Initialize with backend and setup module cleanup
+from . import backend
+import atexit
+
+backend.jheaps_init()
+del backend
+
+
+def _module_cleanup_function():
+    from . import backend
+
+    backend.jheaps_cleanup()
+
+atexit.register(_module_cleanup_function)
+del atexit
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+from . import (
+    types,
+)
+
