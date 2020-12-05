284770e66a38fdfecba415235b550db8b1e55099
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-12-06 01:07:52 +0200

    Added basic travis stuff

---
+++ travis/deploy.sh
@@ -0,0 +1,6 @@
+pyenv global 3.8.2
+pyenv exec python -m pip install -U pip
+pyenv exec python -m pip install twine
+pyenv exec python -m twine --version
+pyenv exec python -m twine upload --username "${PYPI_USER:-__token__}" --password "$PYPI_PASSWORD" dist/*
+
